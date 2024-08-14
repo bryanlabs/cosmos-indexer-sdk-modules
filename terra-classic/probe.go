6d6c3300d2e8c6d60dd03f42f0181af328e97207
pharr117 <pjharral@gmail.com>
2024-08-14 01:37:43 -0400

    Add terra oracle module implementations

--- terra-classic/probe.go
+++ terra-classic/probe.go
@@ -1,7 +1,9 @@
+// This file is not sourced from the licensed code referenced in the LICENSE file of this subfolder. Its licensed by the root LICENSE file.
 package terraclassic
 
 import (
 	marketTypes "github.com/DefiantLabs/cosmos-indexer-modules/terra-classic/x/market/types"
+	oracleTypes "github.com/DefiantLabs/cosmos-indexer-modules/terra-classic/x/oracle/types"
 	wasmTypes "github.com/DefiantLabs/cosmos-indexer-modules/terra-classic/x/wasm/types"
 	"github.com/DefiantLabs/probe/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -20,4 +22,10 @@ func IncludeWASMImplementations(client *client.ChainClient) {
 	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.wasm.v1beta1.MsgStoreCode", (*wasmTypes.MsgStoreCode)(nil))
 	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.wasm.v1beta1.MsgUpdateContractAdmin", (*wasmTypes.MsgUpdateContractAdmin)(nil))
 	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.wasm.v1beta1.MsgClearContractAdmin", (*wasmTypes.MsgClearContractAdmin)(nil))
+}
+
+func IncludeOracleImplementations(client *client.ChainClient) {
+	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.oracle.v1beta1.MsgAggregateExchangeRateVote", (*oracleTypes.MsgAggregateExchangeRateVote)(nil))
+	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.oracle.v1beta1.MsgAggregateExchangeRatePrevote", (*oracleTypes.MsgAggregateExchangeRatePrevote)(nil))
+	client.Codec.ProbeInterfaceRegistry.RegisterCustomTypeURL((*sdk.Msg)(nil), "/terra.oracle.v1beta1.MsgDelegateFeedConsent", (*oracleTypes.MsgDelegateFeedConsent)(nil))
 }
